7fba6fd6241eb8fff7554e93e767a9ca4c664b65
Will Hopper <wjhopper510@gmail.com>
2022-06-24 13:14:07 -0400

    updated imports and licence to fix warnings from R CMD check

--- R/regression_plane.R
+++ R/regression_plane.R
@@ -1,3 +1,10 @@
+#' @importFrom magrittr %>%
+#' @importFrom plotly plot_ly
+#' @importFrom plotly add_markers
+#' @importFrom plotly layout
+#' @importFrom plotly add_trace
+#' @importFrom stats predict
+#'
 regression_plane <- function(model, n_points = 100, mesh=FALSE, mesh_step=1) {
 
   outcome_name <- as.character(model$terms)[[2]]
@@ -9,13 +16,13 @@ regression_plane <- function(model, n_points = 100, mesh=FALSE, mesh_step=1) {
 
   data <- model$model
 
-  p <- plot_ly(x = data[[terms[1]]],
+  p <- plotly::plot_ly(x = data[[terms[1]]],
                y = data[[terms[2]]],
                z = data[[outcome_name]],
                opacity = 0.6
                ) %>%
-    add_markers(marker = list(size=3), showlegend=FALSE, hoverinfo='none') %>%
-    layout(scene = list(camera = list(eye = list(x = -1.25, y = -1.25, z = 1.25)),
+    plotly::add_markers(marker = list(size=3), showlegend=FALSE, hoverinfo='none') %>%
+    plotly::layout(scene = list(camera = list(eye = list(x = -1.25, y = -1.25, z = 1.25)),
                         xaxis = list(title = terms[1]),
                         yaxis = list(title = terms[2]),
                         zaxis = list(title = outcome_name)
@@ -27,7 +34,7 @@ regression_plane <- function(model, n_points = 100, mesh=FALSE, mesh_step=1) {
   predictor.grid <- expand.grid(x, y)
   colnames(predictor.grid) <- terms[!grepl(":", terms)]
 
-  z_hat <- predict(model, newdata = predictor.grid)
+  z_hat <- stats::predict(model, newdata = predictor.grid)
   z_hat <- matrix(z_hat, nrow=n_points, byrow = TRUE)
 
   p <- plotly::add_surface(p, x= ~x, y=~y, z=~z_hat, showlegend=FALSE, hoverinfo='none', showscale = FALSE, hoverinfo='none')
@@ -43,7 +50,7 @@ regression_plane <- function(model, n_points = 100, mesh=FALSE, mesh_step=1) {
     z_hat2 <- matrix(z_hat2, nrow=length(y2), byrow = TRUE)
 
     for (i in 1:ncol(z_hat2)) {
-      p <- p %>% add_trace(type="scatter3d", x=x2[i], y=y2, z=z_hat2[,i],
+      p <- p %>% plotly::add_trace(type="scatter3d", x=x2[i], y=y2, z=z_hat2[,i],
                            mode="lines", line = list(color = "black"),
                            showlegend=FALSE
       )
